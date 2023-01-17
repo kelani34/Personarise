8ea7b45e4def0cc4d5781fa2f577157062e1f114
kelani34 <kelanitaiwo34@gmail.com>
2023-01-17 12:32:35 +0100

    fix: mobile responsiveness for navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import images from "../assets/images";
+
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import styled from "styled-components";
+import "./style.css";
 const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const Menu = () => (
+    <div className="app__sm-nav">
+      <ul>
+        <li>
+          <a>Who we are</a>
+        </li>
+        <li>
+          <a>What we do</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
   return (
     <Wrapper>
       <div className="app__logo-container">
@@ -23,13 +42,34 @@ const Header = () => {
           </ul>
         </div>
       </div>
-
       <div className="app__btn-container">
         <button className="app__btn-demo-btn">
           <img src={images.playIcon} alt="play icon" />
           Watch a demo
         </button>
-        <button className="app__btn-signup-btn">Sign up</button>
+        <button className="app__btn-signup-btn">
+          <div>Sign up</div>
+        </button>
+      </div>
+      <div className="app__sm-container">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#000"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#000"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="app__sm-items">
+            <Menu />
+          </div>
+        )}
       </div>
     </Wrapper>
   );
@@ -38,7 +78,7 @@ const Header = () => {
 export default Header;
 
 const Wrapper = styled.div`
-  max-width: 1140px;
+  max-width: 1400px;
   margin: auto;
   padding: 22px 42px;
   display: flex;
@@ -61,6 +101,9 @@ const Wrapper = styled.div`
       letter-spacing: -0.03em;
 
       color: #000000;
+      @media screen and (max-width: 400px) {
+        font-size: 24px;
+      }
     }
   }
 
@@ -85,6 +128,10 @@ const Wrapper = styled.div`
 
         color: rgba(0, 0, 0, 0.87);
       }
+    }
+
+    @media screen and (max-width: 1150px) {
+      display: none;
     }
   }
   .app__btn-container {
@@ -123,6 +170,47 @@ const Wrapper = styled.div`
       /* identical to box height, or 144% */
 
       color: #ffffff;
+    }
+
+    @media screen and (max-width: 700px) {
+      display: none;
+    }
+  }
+  .app__sm-container {
+    position: relative;
+    .app__sm-nav {
+      position: absolute;
+      right: 0;
+      top: 50px;
+      width: 120px;
+      padding: 20px;
+      background-color: #f4f1f1;
+      border-radius: 8px;
+      z-index: 1;
+      ul {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+        gap: 38px;
+
+        li {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 400;
+          font-size: 18px;
+          line-height: 26px;
+          /* identical to box height, or 144% */
+
+          color: rgba(0, 0, 0, 0.87);
+        }
+      }
+      @media screen and (max-width: 700px) {
+        /* top: 31px; */
+      }
+    }
+
+    @media screen and (min-width: 1150px) {
+      display: none;
     }
   }
   button {
